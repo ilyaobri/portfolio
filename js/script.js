b23fa8598e691b2f98d5f63cd48eb26006bf31b5
Obri <iekoml@mail.ru>
2023-06-15 20:42:27 +0300

    TI-10 Part 1 Sort

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 const skills = {
     data: [{
         item: 'html',
-        level: 40,
+        level: 80,
         iconPath: 'img/skill/html.svg'
     },
     {   
         item: 'css',
-        level: 40,
+        level: 70,
         iconPath: 'img/skill/css.svg'
     },
     {
         item: 'python',
-        level: 40,
+        level: 60,
         iconPath: 'img/skill/python.svg'
     },
     {
         item: 'photoshop',
-        level: 40,
+        level: 50,
         iconPath: 'img/skill/photoshop.svg'
     },
     {
@@ -24,6 +24,7 @@ const skills = {
         level: 40,
         iconPath: 'img/skill/php.svg'
     }],
+    isSorted: false,
     generateList: function(parentElement) {
         parentElement.innerHTML = '';
         this.data.forEach(element => {
@@ -43,7 +44,32 @@ const skills = {
             parentElement.append(dt, dd);
         });
     },
+    sortList: function(sortingType) {
+        if (skills.isSorted !== sortingType) {
+            switch (sortingType) {
+                case 'name':
+                    this.data.sort((a, b) => a.item.localeCompare(b.item)); break;
+                case 'level':
+                    this.data.sort((a, b) => b.level - a.level); break;
+                default:
+                    return;
+            }
+            console.log(`отсортировано по ${sortingType}`);
+            this.isSorted = sortingType;
+        } else {
+            this.data.reverse();
+            console.log('инвертировали порядок сортировки');
+        }
+        this.generateList(skillList);
+    },
 };
 
 const skillList = document.querySelector('dl.skill-list');
 skills.generateList(skillList);
+
+const sortBtnsBlock = document.querySelector('.skill-buttons');
+sortBtnsBlock.addEventListener('click', (e) => {
+    if (e.target.nodeName === "BUTTON") {
+        skills.sortList(e.target.dataset.type);
+    }
+});
